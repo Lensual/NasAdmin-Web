3a12da47cef5b61e8bd5c0d99851fb0fb09b725e
Lensual <lenvisual@gmail.com>
2017-09-05 18:48:31 +0800

    add album

--- plugins/filesystem/script.js
+++ plugins/filesystem/script.js
@@ -1,5 +1,5 @@
 //debug
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 3; i++) {
     document.getElementById("file_manage").innerHTML +=
         document.getElementById("file_manage").innerHTML;
 }
