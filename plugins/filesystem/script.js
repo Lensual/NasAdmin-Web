f04685e9a2d36dca9d73d453153668e1cd107e07
Lensual <lenvisual@gmail.com>
2017-09-20 03:29:39 +0800

    save code

--- plugins/filesystem/script.js
+++ plugins/filesystem/script.js
@@ -3,3 +3,11 @@ for (var i = 0; i < 4; i++) {
     document.getElementById("file_manage_grid").innerHTML +=
         document.getElementById("file_manage_grid").innerHTML;
 }
+
+//function readDirSync() {
+//    getHtml(apiUrl + "/apiFs/readDirSync")
+//        .then(function (result) {
+//            result = 
+//        });
+    
+//}
